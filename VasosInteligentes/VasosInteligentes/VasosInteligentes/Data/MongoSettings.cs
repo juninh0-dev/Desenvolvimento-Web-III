d0ef51cf9c6e7021f3341c5935307f8c76745e0b
Altair Preto de Godoi Junior <altair.jr2006@gmail.com>
2026-04-28 09:55:03 -0300

    Adicionando arquivos de VasosInteligentes, projeto da matéria DSWIII

--- VasosInteligentes/VasosInteligentes/VasosInteligentes/Data/MongoSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VasosInteligentes.Data
-{
-    public class MongoSettings
-    {
-        public string ConnectionString { get; set; }
-        public string Database { get; set; }
-        public bool IsSsl { get; set; }
-    }
-}
--- VasosInteligentes/VasosInteligentes/VasosInteligentes/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VasosInteligentes.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
